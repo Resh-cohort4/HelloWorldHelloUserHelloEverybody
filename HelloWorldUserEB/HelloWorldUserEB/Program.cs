f756562e6fdc0d9c8f56ffbbeb5e34ecb0fd884b
Reshmapraveen <reshmapraveen28@gmail.com>
2020-06-20 21:13:08 -0600

    fixed bug #001

--- HelloWorldUserEB/HelloWorldUserEB/Program.cs
+++ HelloWorldUserEB/HelloWorldUserEB/Program.cs
@@ -2,11 +2,21 @@
 
 namespace HelloWorldUserEB
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static ConsoleKey keyPressed;
+
+        public static void Main()
         {
             Console.WriteLine("Hello World!");
+            while (keyPressed != ConsoleKey.Enter)
+            {
+                Console.WriteLine("\nPress Enter Key to Exit");
+                keyPressed = Console.ReadKey().Key;
+            }
+
         }
+
     }
 }
+
